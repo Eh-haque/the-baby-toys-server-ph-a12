eb8ac6dc1295c25336d6f42ef5c9405d6af45900
Eh-haque <ehtisamul@gmail.com>
2021-11-10 21:38:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ryljg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('assignment_12');
+        const servicesCollection = database.collection('services');
+        const usersCollection = database.collection('users');
+        console.log('database connected');
+
+        // password user update
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        // email user update
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user }
+            const result = await usersCollection.updateOne(query, updateDoc, options);
+            res.send(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user.email }
+            const updateDoc = { $set: {role: 'admin'}};
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('running server...');
+});
+
+app.listen(port, () => {
+    console.log('running server', port);
+});
